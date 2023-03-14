dea8b00776bb6db56212c043519b8fc9858b3e15
Martin Klimeš <k11m1@k11m1.eu>
2023-03-14 10:06:18 +0100

    2 days scheduling

--- org-timed-alerts.el
+++ org-timed-alerts.el
@@ -422,7 +422,7 @@ MESSAGE is the alert body. Optional keys are those accepted by `alert'."
 				(ts-active
 				 :from ,(ts-format "%Y-%m-%d" (ts-now))
 				 :to ,(ts-format "%Y-%m-%d"
-						 (ts-adjust 'day 1 (ts-now)))))
+						 (ts-adjust 'day 2 (ts-now)))))
 			    (not (,@org-timed-alerts-todo-exclusions)))
 			  :action #'org-timed-alerts--org-ql-action)
 	   do (org-timed-alerts--parser entry))
